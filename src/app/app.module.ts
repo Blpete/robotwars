a57ef0e55f41405fe3ef4f940c71dde2079b3e9e
Barry Peterson <blpete@gmail.com>
2020-11-29 19:11:24 -0500

    cleanup, add controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule,
