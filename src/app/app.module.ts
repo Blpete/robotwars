cb70cf2bceb579a8351a1b4a456cee3dfb24fbaf
Barry Peterson <blpete@gmail.com>
2022-12-31 13:56:41 -0500

    eslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,9 @@ import { AppComponent } from './app.component';
 import { ControlsComponent } from './controls/controls.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GameboardService } from './gameboard.service';
-import { PhaserModule } from 'phaser-component-library';
 import { HomepageComponent } from './homepage/homepage.component';
 import { EntitytileComponent } from './entitytile/entitytile.component';
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
@@ -22,9 +19,7 @@ const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
     EntitytileComponent
   ],
   imports: [
-    PhaserModule,
     BrowserModule,
-    SocketIoModule.forRoot(config),
     AppRoutingModule
   ],
   providers: [GameboardService],
